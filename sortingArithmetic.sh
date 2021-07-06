53aa7ab44da32da1ec5272f95be58e54c3c2c4f7
SAURABH SHIRSATH <saurabhpshirsath@gmail.com>
2021-07-06 21:42:40 +0530

    UC-1_Take3Inputs

--- sortingArithmetic.sh
+++ sortingArithmetic.sh
@@ -1,1 +1,6 @@
-Welcome to Arithmetic Computation and sorting
+#!/bin/bash -x
+echo "Welcome to Sorting Arithmetic Computaions"
+echo "Taking 3 inputs"
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+read -p "Enter third number: " num3
