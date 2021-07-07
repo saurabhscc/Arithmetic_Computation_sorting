8984dbe41315cd346aca8cb33afdf5e519b01599
SAURABH SHIRSATH <saurabhpshirsath@gmail.com>
2021-07-07 10:23:49 +0530

    UC-6_StoreResultsDictionary

--- sortingArithmetic.sh
+++ sortingArithmetic.sh
@@ -8,3 +8,9 @@ sol1=$(($num1+$num2*$num3));
 sol2=$(($num1*$num2+$num3));
 sol3=$(($num3+$num1/$num2));
 sol4=$(($num1%$num2+$num3));
+
+declare -A computation
+computation[0]=$sol1;
+computation[1]=$sol2;
+computation[2]=$sol3;
+computation[3]=$sol4;
